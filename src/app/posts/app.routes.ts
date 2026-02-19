1ca2ff70e8090421ccd19199cfb815b22456ff8f
Angel Dadole <adadole17580@liceo.edu.ph>
2026-02-19 16:19:37 +0800

    Updated Angular Activity 1

--- src/app/posts/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { PostsComponent } from './posts.component';
-
-export const routes: Routes = [
-  { path: 'posts', component: PostsComponent }
-];
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -2,27 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-posts',
+  standalone: true,
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.scss']
+  styleUrls: ['./posts.component.css']
 })
-export class PostsComponent {
-
-  posts = [
-    {
-      title: 'Latest Updates',
-      description: 'New features and improvements have been released.'
-    },
-    {
-      title: 'Community Post',
-      description: 'Welcome to our growing community!'
-    },
-    {
-      title: 'Announcement',
-      description: 'Scheduled maintenance this weekend.'
-    }
-  ];
-
-  deletePost(index: number) {
-    this.posts.splice(index, 1);
-  }
-}
+export class PostsComponent {}
